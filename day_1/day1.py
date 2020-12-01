d030964f847365046d3e8391543f0fddea273ffc
9harkonnen6 <9harkonnen6@gmail.com>
2020-12-01 14:15:29 +0100

    fix: final version of 1st day of AOC2020

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,8 +1,21 @@
 expenses = []
+results = []
 for c in open("day_1/expenses.txt"):
     expenses.append(int(c))
 
+# 1st task
 for i in expenses:
     for j in expenses:
         if i+j == 2020:
-            print(i*j)
+                if i*j not in results:
+                    results.append(i*j)
+print(results)
+results.clear()
+# 2nd task
+for i in expenses:
+    for j in expenses:
+        for k in expenses:
+            if i+j+k == 2020:
+                if i*j*k not in results:
+                    results.append(i*j*k)
+print(results)
