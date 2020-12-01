a95953fb6483d9d7309094fa69a9fc9ff35aaea6
9harkonnen6 <9harkonnen6@gmail.com>
2020-12-01 14:01:51 +0100

    fix: correct minor typos

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,8 +1,8 @@
 expenses = []
-for c in open(f"expenses.txt"):
+for c in open("day_1/expenses.txt"):
     expenses.append(int(c))
 
 for i in expenses:
     for j in expenses:
         if i+j == 2020:
-            print(f'{i} + {j} = 2020')
+            print(i*j)
