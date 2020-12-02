07fa8d5555e04eba9f484fb458d2dee99c090495
9harkonnen6 <9harkonnen6@gmail.com>
2020-12-02 23:16:04 +0100

    2nd day of AOC2020

--- day_1/day1.py
+++ day_1/day1.py
@@ -30,6 +30,10 @@ while iterator < len(expenses):
         else:
             pass
 
+choosen_number = 0
+choosen_number_2 = 0
+iterator = 0
+
 # 2nd task, optimalization is literally non-existing coz of three loops
 for i in expenses:
     for j in expenses:
@@ -38,3 +42,20 @@ for i in expenses:
                 if i*j*k not in results:
                     results.append(i*j*k)
 print(results)
+
+# 2nd task, optimalised
+
+while iterator < len(expenses):
+    if expenses[choosen_number]+expenses[iterator] == 2020:
+        print(f'{expenses[choosen_number]}+{expenses[iterator]}=2020')
+        print(expenses[choosen_number]*expenses[iterator])
+        break
+    else:
+        iterator +=1
+        if iterator == 200:
+            choosen_number +=1
+            iterator = 0
+        elif iterator == choosen_number:
+            iterator +=1
+        else:
+            pass
